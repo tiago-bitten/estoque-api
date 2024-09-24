244ca83f9691d8ffd5db279b317e56d4cdd4e8bd
Tiago <tiago.sbittencourt@icloud.com>
2024-09-24 00:46:08 -0300

    paged result

--- SistemaEstoque.Application/Queries/PagedResponse.cs
+++ SistemaEstoque.Application/Queries/PagedResponse.cs
@@ -4,11 +4,11 @@ namespace SistemaEstoque.Application.Queries
 {
     public abstract class PagedResponse<T> where T : class
     {
-        public PagedResult<T> Result { get; set; }
+        public PagedResult<T> PagedResult { get; set; }
 
         protected PagedResponse(PagedResult<T> result)
         {
-            Result = result;
+            PagedResult = result;
         }
     }
 }
